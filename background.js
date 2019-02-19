7e049241f60fad3ef8482c21e2918b9227081581
MehdiBenbahri <taoufikvar@gmailcom>
2019-02-19 14:46:39 +0100

    -Corrections du bug de lancement

--- background.js
+++ background.js
@@ -1,12 +1,7 @@
-'use strict';
-chrome.storage.sync.get(['avoui'], function(check) {
-    if (check.avoui === null) {
-        chrome.runtime.onInstalled.addEventListener()(function() {
-            chrome.tabs.create({
-                url: "option.html"
-            }, function(tab) {
-                chrome.tts.speak('Êtes-vous atteint d\'une cécité visuelle totale ? Merci de cocher le bouton correspondant. Vous pouvez aussi naviguer avec la touche tabulation et les touches fléchées.');
-            });
-        });
-    }
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.tabs.create({
+        url: "option.html"
+    }, function(tab) {
+        chrome.tts.speak('Êtes-vous atteint d\'une cécité visuelle totale ? Merci de cocher le bouton correspondant. Vous pouvez aussi naviguer avec la touche tabulation et les touches fléchées.');
+    });
 });
